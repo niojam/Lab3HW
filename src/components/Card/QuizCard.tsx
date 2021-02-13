995576c75088acfbd37585f4d647d206fd697746
niojam <niojam@ttu.ee>
2021-02-13 16:57:30 +0200

    #19 Quiz card implemented

--- src/components/Card/QuizCard.tsx
+++ src/components/Card/QuizCard.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { Avatar, Card } from "antd";
+import "./QuizCard.scss";
+import Meta from "antd/es/card/Meta";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  CaretRightOutlined,
+} from "@ant-design/icons";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface QuizCardProps {}
+interface QuizCardProps {
+  title: string;
+  description: string;
+  coverImg: ReactNode;
+}
 
-const QuizCard = ({}: QuizCardProps) => {
-  return <>tere</>;
+const QuizCard = ({ title, description, coverImg }: QuizCardProps) => {
+  return (
+    <Card
+      className={"quiz-card"}
+      style={{ width: 280 }}
+      cover={coverImg}
+      actions={[
+        <CaretRightOutlined className={"quiz-card--button"} key="playQuiz" />,
+        <EditOutlined className={"quiz-card--button"} key="editQuiz" />,
+        <EllipsisOutlined className={"quiz-card--button"} key="ellipsis" />,
+      ]}
+    >
+      <Meta title={title} description={description} />
+    </Card>
+  );
 };
 
 export default QuizCard;
