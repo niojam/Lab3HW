b3b570068262a3fc87efa1b94f61b6a560ef67e0
nemens1s <ilja.nikolski@hotmail.com>
2021-02-20 19:45:54 +0200

    #16 Quizzes view - added props to StatisticsOverviewTable.tsx - data is know in PlayedQuizzes.tsx

--- src/containers/Statistics/StatisticsOverviewTable.tsx
+++ src/containers/Statistics/StatisticsOverviewTable.tsx
@@ -1,63 +1,16 @@
 import React from "react";
-import { Space } from "antd";
-import { Icon } from "components";
-import { Users, Download, Remove, Chart } from "../../assets/images";
 import { GenericTable } from "../../components";
 import "./StatisticsOverviewTable.scss";
-const data = [
-  {
-    key: "1",
-    quizName: "What is OOP",
-    roomName: "Room number 1",
-    time: "25.08.2020",
-  },
-  {
-    key: "2",
-    quizName: "What is OOP",
-    roomName: "Room number 2",
-    time: "26.08.2020",
-  },
-  {
-    key: "3",
-    quizName: "What is OOP",
-    roomName: "Room number 3",
-    time: "28.08.2020",
-  },
-];
 
-const columns = [
-  {
-    title: "Quiz Name",
-    dataIndex: "quizName",
-    key: "quizName",
-  },
-  {
-    title: "Room Name",
-    dataIndex: "roomName",
-    key: "roomName",
-  },
-  {
-    title: "Time",
-    dataIndex: "time",
-    key: "time",
-  },
-  {
-    title: "",
-    key: "action",
-    render: function renderIcons() {
-      return (
-        <Space size="middle">
-          <Icon src={Chart} size={"small"} />
-          <Icon src={Users} size={"small"} />
-          <Icon src={Download} size={"small"} />
-          <Icon src={Remove} size={"small"} />
-        </Space>
-      );
-    },
-  },
-];
+interface StatisticsOverviewTableProps {
+  data: Array<any>;
+  columns: Array<any>;
+}
 
-const StatisticsOverviewTable = () => {
+const StatisticsOverviewTable = ({
+  data,
+  columns,
+}: StatisticsOverviewTableProps) => {
   return <GenericTable tableData={data} columns={columns} />;
 };
 
--- src/views/PlayedQuizzes/PlayedQuizzes.tsx
+++ src/views/PlayedQuizzes/PlayedQuizzes.tsx
@@ -1,7 +1,62 @@
 import React from "react";
 import StatisticsOverviewTable from "../../containers/Statistics/StatisticsOverviewTable";
-import { Col, Pagination, Row } from "antd";
+import { Col, Pagination, Row, Space } from "antd";
+import { Icon } from "components";
+import { Users, Download, Remove, Chart } from "../../assets/images";
 import "./PlayedQuizzes.scss";
+
+const data = [
+  {
+    key: "1",
+    quizName: "What is OOP",
+    roomName: "Room number 1",
+    time: "25.08.2020",
+  },
+  {
+    key: "2",
+    quizName: "What is OOP",
+    roomName: "Room number 2",
+    time: "26.08.2020",
+  },
+  {
+    key: "3",
+    quizName: "What is OOP",
+    roomName: "Room number 3",
+    time: "28.08.2020",
+  },
+];
+
+const columns = [
+  {
+    title: "Quiz Name",
+    dataIndex: "quizName",
+    key: "quizName",
+  },
+  {
+    title: "Room Name",
+    dataIndex: "roomName",
+    key: "roomName",
+  },
+  {
+    title: "Time",
+    dataIndex: "time",
+    key: "time",
+  },
+  {
+    title: "",
+    key: "action",
+    render: function renderIcons() {
+      return (
+        <Space size="middle">
+          <Icon src={Chart} size={"small"} />
+          <Icon src={Users} size={"small"} />
+          <Icon src={Download} size={"small"} />
+          <Icon src={Remove} size={"small"} />
+        </Space>
+      );
+    },
+  },
+];
 
 const PlayedQuizzes = () => {
   return (
@@ -12,7 +67,7 @@ const PlayedQuizzes = () => {
       className={"div-container"}
     >
       <Col md={24} lg={18}>
-        <StatisticsOverviewTable />
+        <StatisticsOverviewTable data={data} columns={columns} />
         <Pagination
           defaultCurrent={1}
           defaultPageSize={10}
