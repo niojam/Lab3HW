e449f4d59b7bb48a47dd512caa281cd8fc3fdf2f
nemens1s <ilja.nikolski@hotmail.com>
2021-02-21 22:57:59 +0200

    #14 Player Statistics view - added PlayerStatisticsTable

---
+++ src/containers/Statistics/PlayerStatisticsTable.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { GenericTable } from "../../components";
+import "./common.scss";
+
+interface PlayerStatisticsTableProps {
+  data: Array<any>;
+  columns: Array<any>;
+}
+
+const PlayerStatisticsTable = ({
+  data,
+  columns,
+}: PlayerStatisticsTableProps) => {
+  return (
+    <GenericTable
+      tableData={data}
+      columns={columns}
+      className={"table-style"}
+    />
+  );
+};
+
+export default PlayerStatisticsTable;
