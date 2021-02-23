85bc82ebbaab81accc1eb93c076a08f3d0ad9552
niojam <niojam@ttu.ee>
2021-02-23 19:12:23 +0200

    #28 Proxy fixed

--- src/common/client/BackOfficeApplicationClient.ts
+++ src/common/client/BackOfficeApplicationClient.ts
@@ -1,10 +1,16 @@
 import axios, { AxiosResponse } from "axios";
-import { QuizDetails } from "../type/Types";
+import { Quiz, QuizDetails } from "../type/Types";
 
-export const getQuizzes = async (): Promise<AxiosResponse<QuizDetails[]>> => {
-  return await axios.get(`api/quiz/quizzes`);
+export const getQuizzesDetails = async (): Promise<
+  AxiosResponse<QuizDetails[]>
+> => {
+  return await axios.get(`/api/quiz/quizzes`);
+};
+
+export const getQuiz = async (quizId: string): Promise<AxiosResponse<Quiz>> => {
+  return await axios.get(`/api/quiz?quizId=${quizId}`);
 };
 
 export const magicLogin = async (): Promise<AxiosResponse> => {
-  return await axios.get(`api/magic-login`);
+  return await axios.get(`/api/magic-login`);
 };
