a0f613099e0ff133a1b278984b53094c453942f7
nemens1s <ilja.nikolski@hotmail.com>
2021-02-27 18:45:42 +0200

    #32 Played quizzes view functionality - Added fetching from back-end - Changed data type in StatisticsOverviewTable from Array<any> to AuthorQuizzes[]

--- src/views/PlayedQuizzes/PlayedQuizzes.tsx
+++ src/views/PlayedQuizzes/PlayedQuizzes.tsx
@@ -1,68 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatisticsOverviewTable } from "containers";
 import { Col, Pagination, Row, Space } from "antd";
 import { Icon } from "components";
 import { Users, Download, Remove, Chart } from "assets/images";
 import "./PlayedQuizzes.scss";
-
-const data = [
-  {
-    key: "1",
-    quizName: "What is OOP",
-    roomName: "Room number 1",
-    time: "25.08.2020",
-  },
-  {
-    key: "2",
-    quizName: "What is OOP",
-    roomName: "Room number 2",
-    time: "26.08.2020",
-  },
-  {
-    key: "3",
-    quizName: "What is OOP",
-    roomName: "Room number 3",
-    time: "28.08.2020",
-  },
-];
-
-const columns = [
-  {
-    title: "Quiz Name",
-    dataIndex: "quizName",
-    key: "quizName",
-  },
-  {
-    title: "Room Name",
-    dataIndex: "roomName",
-    key: "roomName",
-  },
-  {
-    title: "Time",
-    dataIndex: "time",
-    key: "time",
-  },
-  {
-    title: "",
-    key: "action",
-    render: function renderIcons() {
-      return (
-        <Space size="middle">
-          <Icon src={Chart} size={"small"} />
-          <Icon src={Users} size={"small"} />
-          <Icon src={Download} size={"small"} />
-          <Icon src={Remove} size={"small"} />
-        </Space>
-      );
-    },
-  },
-];
+import { AuthorQuizzes } from "../../common/type/Types";
+import { useQuery } from "react-query";
+import { getAuthorQuizzes } from "../../common/client/BackOfficeApplicationClient";
 
 const PlayedQuizzes = () => {
+  const columns = [
+    {
+      title: "Quiz Name",
+      dataIndex: "quizName",
+      key: "quizName",
+    },
+    {
+      title: "Room Name",
+      dataIndex: "roomName",
+      key: "roomName",
+    },
+    {
+      title: "Time",
+      dataIndex: "time",
+      key: "time",
+    },
+    {
+      title: "",
+      key: "action",
+      render: function renderIcons() {
+        return (
+          <Space size="middle">
+            <Icon src={Chart} size={"small"} />
+            <Icon src={Users} size={"small"} />
+            <Icon src={Download} size={"small"} />
+            <Icon src={Remove} size={"small"} />
+          </Space>
+        );
+      },
+    },
+  ];
+  const [rooms, setData] = useState<AuthorQuizzes[]>([]);
+  const {} = useQuery("getAuthorQuizzes", getAuthorQuizzes, {
+    staleTime: 10000,
+    refetchOnWindowFocus: false,
+    retry: false,
+    onSuccess: (result) => {
+      const data: AuthorQuizzes[] = [];
+      for (let i = 0; i < result.data.length; i++) {
+        const authorQuiz = result.data[i];
+        authorQuiz.key = i + 1;
+        data.push(authorQuiz);
+      }
+      setData(data);
+    },
+  });
+
   return (
     <Row justify={"center"} align={"middle"}>
       <Col md={24} lg={18} className={"m-3"}>
-        <StatisticsOverviewTable data={data} columns={columns} />
+        <StatisticsOverviewTable data={rooms} columns={columns} />
         <Pagination
           defaultCurrent={1}
           defaultPageSize={10}
