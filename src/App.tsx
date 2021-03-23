6bc1244cb23a54f77a914df0e1c85f179ac37166
niojam <niojam@ttu.ee>
2021-03-23 16:51:52 +0200

    #50 Added authentication with token

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "antd/dist/antd.css";
 import "./index.scss";
 import { routes } from "./router/config";
 import CustomRouter from "./router/CustomRouter";
-import { useSelector } from "react-redux";
-import { isUserAuthenticated } from "./store/AuthenticationSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  isUserAuthenticated,
+  setIsUserAuthenticated,
+} from "./store/AuthenticationSlice";
+import { useCookies } from "react-cookie";
 
+const KAHOOT_TOKEN_COOKIE = "kahoot-back-office_refresh-token";
 const App = () => {
+  const dispatch = useDispatch();
+  const [cookies] = useCookies([KAHOOT_TOKEN_COOKIE]);
   const authenticated = useSelector(isUserAuthenticated);
 
-  return <CustomRouter routes={routes} isAuthenticated={authenticated} />;
+  useEffect(() => {
+    if (cookies[KAHOOT_TOKEN_COOKIE]) {
+      dispatch(setIsUserAuthenticated(true));
+    }
+  }, [cookies]);
+
+  return (
+    <CustomRouter
+      routes={routes}
+      isAuthenticated={
+        authenticated ? authenticated : !!cookies[KAHOOT_TOKEN_COOKIE]
+      }
+    />
+  );
 };
 
 export default App;
