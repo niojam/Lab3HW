1f4a212f036ee6bab7368fbe5a66db873046aef9
niojam <niojam@ttu.ee>
2021-03-30 16:11:10 +0300

    #61 Landing page images responsive layout implemented

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,26 @@
 import React from "react";
+import "./Home.scss";
+import { Col, Image, Row } from "antd";
+import { TalTechLogoWithBackground } from "../../assets/images";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Row>
+        <Col className={"main-page__img-container"} span={24} />
+        <Col span={24}>
+          <Row>
+            <Col offset={2} span={8}>
+              <Image
+                className={"main-page__logo"}
+                src={TalTechLogoWithBackground}
+                preview={false}
+              />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
   );
 };
 
